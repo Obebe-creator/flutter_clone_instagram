d35d0e598b11ba85fd8cb6a9921cbdde3c56a5bf
Obebe <shkim000905@naver.com>
2023-08-10 20:47:12 +0900

    인스타그램 클론 1

---
+++ lib/src/app.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_clone_inst/src/components/image_data.dart';
+import 'package:flutter_clone_inst/src/controller/bottom_nav_controller.dart';
+import 'package:get/get.dart';
+
+class App extends GetView<BottomNavController> {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: controller.willPopAction,
+      child: Obx(
+        () => Scaffold(
+          appBar: AppBar(),
+          body: IndexedStack(
+            index: controller.pageIndex.value,
+            children: [
+              Container(
+                child: Center(child: Text('HOME')),
+              ),
+              Container(
+                child: Center(child: Text('SEARCH')),
+              ),
+              Container(
+                child: Center(child: Text('UPLOAD')),
+              ),
+              Container(
+                child: Center(child: Text('ACTIVITY')),
+              ),
+              Container(
+                child: Center(child: Text('MYPAGE')),
+              ),
+            ],
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            currentIndex: controller.pageIndex.value,
+            elevation: 0,
+            onTap: controller.changeBottomNav,
+            items: [
+              BottomNavigationBarItem(
+                icon: ImageData(icon: IconsPath.homeOff),
+                activeIcon: ImageData(icon: IconsPath.homeOn),
+                label: 'home',
+              ),
+              BottomNavigationBarItem(
+                icon: ImageData(icon: IconsPath.searchOff),
+                activeIcon: ImageData(icon: IconsPath.searchOn),
+                label: 'home',
+              ),
+              BottomNavigationBarItem(
+                icon: ImageData(icon: IconsPath.uploadIcon),
+                label: 'home',
+              ),
+              BottomNavigationBarItem(
+                icon: ImageData(icon: IconsPath.activeOff),
+                activeIcon: ImageData(icon: IconsPath.activeOn),
+                label: 'home',
+              ),
+              BottomNavigationBarItem(
+                icon: Container(
+                  width: 30,
+                  height: 30,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.grey,
+                  ),
+                ),
+                label: 'home',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/pages/upload.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Upload extends StatelessWidget {
+  const Upload({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey,
+    );
+  }
+}
